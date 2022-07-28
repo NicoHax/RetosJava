09add3e545eaa945a32422d05b52f30a423fc74a
NicoHax <nickolasch2811@gmail.com>
2022-07-28 12:16:45 -0500

    Se añade el calcular_interes

--- Reto2.java
+++ Reto2.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 class Cuenta {
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         double Interes, SaldoCuenta, SaldoFinal, Total_Interes = 0, Total_Saldo = 0, InteresMensual;
         int CantidadUsuarios = Integer.parseInt(entrada.nextLine()), TipoCuenta;
         long NoCuenta;
-        String FechaApertura = null;
         for (int i=0;i<CantidadUsuarios;i++){
             NoCuenta = entrada.nextLong();
-            FechaApertura = entrada.next();
+            String FechaApertura = entrada.next();
             TipoCuenta = entrada.nextInt();
+            float calcular_interes = 0; {
             switch (TipoCuenta) {
                 case 1: Interes = 0.015;
                     break;
@@ -21,16 +21,15 @@ class Cuenta {
                 default: Interes = 7500;
                     break;}
             SaldoCuenta = entrada.nextDouble();
-            InteresMensual = SaldoCuenta * Interes;
+            InteresMensual = SaldoCuenta * Interes; }
             SaldoFinal = SaldoCuenta + InteresMensual;
             Total_Interes = Total_Interes + InteresMensual;
             Total_Saldo = Total_Saldo + SaldoFinal;
-            float FloatInteresMensual = Float.parseFloat(String.valueOf((InteresMensual)));
             System.out.println(NoCuenta);
-            System.out.println(FloatInteresMensual);
+            System.out.println(calcular_interes);
             System.out.println(SaldoFinal);}
         System.out.println(Total_Interes);
         System.out.println(Total_Saldo);
-    }}
+        }}
 
 
